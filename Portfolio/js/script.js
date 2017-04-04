d44533e8a08f40817085d4e91e4efebca4b95de3
Damyon Jackson <damyonjackson@gmail.com>
2017-04-04 08:14:41 -0500

    Add files via upload

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -4,8 +4,36 @@ $(document).ready(function () {
     $('#construction').modal('show');
 
 });
-$(function(){
 
+$(function(){
+// Set the date we're counting down to
+var countDownDate = new Date("July 22, 2017 16:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="wedding_date"
+    document.getElementById("wedding_date").innerHTML = days + " days, " + hours + " hours, "
+    + minutes + " minutes, & " + seconds + " seconds ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("wedding_date").innerHTML = "EXPIRED";
+    }
+}, 1000);
 
 //notification bar
     $('.notification-bar').delay(3000).slideDown().delay(5000).slideUp();
